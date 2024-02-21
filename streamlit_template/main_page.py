238308ec9055981e04a61557cf3012b7dcfc884e
10802437 <157433752+10802437@users.noreply.github.com>
2024-02-20 21:35:54 -0700

    fixed

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer or kart stats")
 
-link = 'https://10802437.github.io/Github_Assignment/'
+link = '[Link to my GitHub Pages Site]https://10802437.github.io/Github_Assignment/'
 st.markdown(link, unsafe_allow_html=True)
